02286515a12324867a77c6654d5cc716d3eab6a4
sentic0mm <morimann2013@gmail.com>
2026-06-19 16:05:20 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+
+const CACHE='slots-v1';
+self.addEventListener('install',e=>{
+e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./sw.js'])));
+});
+self.addEventListener('fetch',e=>{
+e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
