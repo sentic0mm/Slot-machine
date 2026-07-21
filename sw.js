e34f41e86cf84b16c32b84b31359f4ff8c5184b7
Sentic0m <rezoderzerstoerer64@gmail.com>
2026-07-21 21:13:33 +0200

    neue cache version

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Cache-Version: bei jedem größeren Update hochzählen (v2 -> v3 ...).
 // Das erzwingt, dass alte Caches gelöscht werden.
-const CACHE = 'slots-v34';
+const CACHE = 'slots-v35';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
